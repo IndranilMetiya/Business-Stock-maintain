3a9ffbdefcfe08fdfed07823442bc8271e08776c
Indra <indranilmetiya7@gmail.com>
2024-03-02 22:42:51 +0530

    CORS added

--- src/main/java/com/Business/controller/Controller.java
+++ src/main/java/com/Business/controller/Controller.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,13 +31,15 @@ public class Controller {
 	
 	@Autowired
 	DaoService daoService;
-	
+		
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @PostMapping("/createCatagory")
 	    public ResponseEntity<String> createCatagory(@RequestBody CategoryDTO catagoryDto) {
 	    String msg	=  daoService.createCatagory(catagoryDto);
 	        return ResponseEntity.ok(msg);
 	}
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @GetMapping("/getCategories")
 	   public ResponseEntity<List<CategoryDTO>> getCategories(){
 		   
@@ -44,66 +47,77 @@ public class Controller {
 		   return ResponseEntity.ok(categoryDTOs);
 	   }
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @PostMapping("/createSubCatagory")
 	   public ResponseEntity<String> createSubCatagory(@RequestBody SubCategoryDTO subCategoryDTO) {
 		    String msg	=  daoService.createSubCategory(subCategoryDTO);
 		        return ResponseEntity.ok(msg);
 		}
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @GetMapping("/getSubCategories")
 	   public ResponseEntity<List<SubCategoryDTO>> getSubCategories(){	   
 		   List<SubCategoryDTO> subCategoryDTOs=daoService.getAllSubCategories();
 		   return ResponseEntity.ok(subCategoryDTOs);
 	   }
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @PostMapping("/getSubCategoriesByCategory")
 	   public ResponseEntity<List<SubCategoryDTO>> getSubCategoriesByCategory(@RequestBody CategoryDTO categoryDTO){	   
 		   List<SubCategoryDTO> subCategoryDTOs=daoService.getAllSubCategoryFromAcategory(categoryDTO);
 		   return ResponseEntity.ok(subCategoryDTOs);
 	   }
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @PostMapping("/getAllSubCategoryFromAcategoryAsList")
 	    public ResponseEntity<List<String>> getAllSubCategoryFromAcategoryAsList(@RequestBody CategoryDTO catagoryDto) {
 		   List<String> msg	=  daoService.getAllSubCategoryFromAcategoryAsList(catagoryDto);
 	        return ResponseEntity.ok(msg);
 	}
-	   
+	  
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @PostMapping("/createProperty")
 	   public ResponseEntity<String> createProperty(@RequestBody PropertyDTO propertyDTO) {
 		    String msg	=  daoService.createProperty(propertyDTO);
 		        return ResponseEntity.ok(msg);
 		}
 
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @GetMapping("/getProperties")
 	   public ResponseEntity<List<PropertyDTO>> getProperties(){	   
 		   List<PropertyDTO> propertyDTOs=daoService.getProperties();
 		   return ResponseEntity.ok(propertyDTOs);
 	   }
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @GetMapping("/getStocks")
 	   public ResponseEntity<List<StockDTO>> getStocks(){	   
 		   List<StockDTO> stockDTOs=daoService.getStocks();
 		   return ResponseEntity.ok(stockDTOs);
 	   }
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @PostMapping("/createTransactionData")
 	   public ResponseEntity<String> createTransactionData(@RequestBody TransactionDTO tDTO) {
 		    String msg	=  daoService.createTransactionData(tDTO);
 		        return ResponseEntity.ok(msg);
 		}
 
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @DeleteMapping("/deleteTransactionData")
 	   public ResponseEntity<String> deleteTransactionData(@RequestBody TransactionDTO tDTO) {
 		    String msg	=  daoService.deleteTransactionData(tDTO);
 		        return ResponseEntity.ok(msg);
 		}
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @PutMapping("/updateTransactionData")
 	   public ResponseEntity<String> updateTransactionData(@RequestBody TransactionDTO tDTO) {
 		    String msg	=  daoService.updateTransactionData(tDTO);
 		        return ResponseEntity.ok(msg);
 		}
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @GetMapping("/getTransactionData")
 	   public ResponseEntity<List<TransactionDTO>> getTransactionData(){		   
 		   List<TransactionDTO> tDtos= new ArrayList<>();
@@ -112,12 +126,14 @@ public class Controller {
 		   
 	   }	
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @PostMapping("/createPriceTrackerData")
 	   public ResponseEntity<String> createPriceTrackerData(@RequestBody PriceTrackerDTO pDto) {
 		    String msg	=  daoService.createPriceTrackerData(pDto);
 		        return ResponseEntity.ok(msg);
 		}
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @GetMapping("/getAllPrices")
 	   public ResponseEntity<List<PriceTrackerDTO>> getAllPrices(){		   
 		   List<PriceTrackerDTO> tDtos= new ArrayList<>();
@@ -126,6 +142,7 @@ public class Controller {
 		   
 	   }
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @PostMapping("/getAllPricesbyProperty")
 	   public ResponseEntity<List<PriceTrackerUpdateDTO>> getAllPricesbyProperty(@RequestBody String propertyName) {
 		   List<PriceTrackerUpdateDTO> pList	=  daoService.getAllPricesbyProperty(propertyName);
@@ -133,6 +150,7 @@ public class Controller {
 		}
 	   
 	   
+	   @CrossOrigin(origins = "*", allowedHeaders = "*")
 	   @GetMapping("/getAllCustomers")
 	   public ResponseEntity<List<Customer>> getAllPricesbyProperty() {
 		   List<Customer> customers	=  daoService.getAllCustomer();
